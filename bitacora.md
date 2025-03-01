d943eddc6c729dd92e3992ff4de812f1460251e8
jair ortiz <bgpolmaquina@gmail.com>
2025-03-01 17:32:34 -0500

    v0.3

---
+++ bitacora.md
@@ -0,0 +1,15 @@
+# Problema 1 
+Crear un repositorio local.
+# Problema 2 
+Crear un repositorio remoto.
+Sincronizar los repositorios.
+Configurar user.name  y user.email
+# Problema  3 
+Crear una pàgina que muestre un mapa centrado en mi barrio 
+## Solucion propuesta
+Utilizar una biblioteca de visor geogràfico: Leaflet, open layers, mapopen
+#  Problema 4
+Mostrar el poligono de mi barrio 
+# posible solucion 
+Buscar geojson de terceros.
+Cargar el geojson usando async await
--- programa.js
+++ programa.js
@@ -4,3 +4,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+async function loadPolygon (){
+    let myData = await fetch('El_Pesebre.geojson');
+    let myPolygon = await myData.json();
+        L.geoJSON(myPolygon,
+        {
+            style:{
+                color:'blue',
+                
+            }
+        }
+    ).addTo(map);
+}
+loadPolygon();
