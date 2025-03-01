1a854a906fa0793bd327ebc4542e3d5da24f2a9c
jair ortiz <bgpolmaquina@gmail.com>
2025-03-01 17:10:04 -0500

    v0.2

---
+++ programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.574366807166772, -74.11646968009038], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
